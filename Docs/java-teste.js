a1b489059a2133f0c9edab59b1032b4395fcce1c
gabrielcbe <gabrielcbe@hotmail.com>
2019-06-12 10:28:43 -0300

    teste

---
+++ Docs/java-teste.js
@@ -0,0 +1,21 @@
+function exibeTexto(valor){
+
+    alert(valor)
+
+}
+exibeTexto('Este é um Texto');
+
+
+function calcula(a,b){
+
+    alert((a+b)/2)
+
+}
+calcula(6,15);
+
+function minhaFuncao(numeroLados){
+    for(i=0;i<numeroLados;i++){
+            alert(numeroLados - i);
+    }
+}
+calcula(6,15);
