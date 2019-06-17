7db271a0f3ef49b9ccaa05fcc612ebb7fd7bcb8d
Gabriel Becheleni <43211813+gabrielcbe@users.noreply.github.com>
2019-06-17 16:20:40 -0300

    Update mbotx-teste.js

--- scratchx-mbot/mbotx-teste.js
+++ scratchx-mbot/mbotx-teste.js
@@ -1,5 +1,5 @@
 (function(ext) {
-	//version=1.6 ja tinha um 1.7 entao devia ser 1.9 se nao tiver atrapalhado nada
+	//version=1.6 ja tinha um 1.7 entao devia ser 2.0 se nao tiver atrapalhado nada
 	var socket = null;
 	var connected = false;
 	var myStatus = 1; // initially yellow
